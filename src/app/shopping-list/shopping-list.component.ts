2caadbc3d4d543d8c08f2aed2c020aada69c504b
AndreiBirlan <barlan.andrei@yahoo.com>
2020-07-10 17:24:53 +0300

    first commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { LoggingService } from '../logging.service';
+import * as ShoppingListActions from './store/shopping-lists.actions';
+import * as fromApp from '../store/app.reducer';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
+  private subscription: Subscription;
+
+  constructor(private loggingService: LoggingService,
+              private store: Store<fromApp.AppState>
+  ) {}
+
+  ngOnInit() {
+    this.ingredients = this.store.select('shoppingList');
+
+    this.loggingService.printLog('Hello from shoppingListComponent ngOnInit');
+  }
+
+  onEditItem(index: number){
+    this.store.dispatch(new ShoppingListActions.StartEdit(index))
+  }
+
+  ngOnDestroy() {}
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyDvHrCN6gsg8yfhK_zpYxUy4WBSJV5C9vE',
+};
